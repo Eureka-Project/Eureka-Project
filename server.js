f6e67a0bc358d92342d5ee32b7225bd4f83d4edf
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-16 14:14:00 -0600

    Added mongoose, body-parser, and cookie-parser

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var mongoose    = require('mongoose');
+
 var app = express();
 
+// Change this url later.
+mongoose.connect('mongodb://localhost/eureka');
+
 app.set('port', (process.env.PORT || 3000));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function(){
 	console.log('Node app is running on port', app.get('port'));
 });
+
+module.exports = app;

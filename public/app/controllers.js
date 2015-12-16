63ba546fd69735922b1eb751f670fc25279e2a24
Hank Couture <hankcouture@gmail.com>
2015-12-16 16:41:44 -0600

    Displayed Fake Content on Home Page for Styling/Sorting

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -3,8 +3,9 @@ var Home = angular.module('eureka.home', [])
 Home.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 
 	$scope.links = [
-	{ title:"Let’s not let fear defeat our values",source:"Medium",url:"https://medium.com/@sundar_pichai/let-s-not-let-fear-defeat-our-values-af2e5ca92371#.cfnq2efuy" },
-	{ title:"Google’s Angular 2 Framework Hits Beta",source:"TechCrunch",url:"http://techcrunch.com/2015/12/15/googles-angular-2-framework-hits-beta/" }
+	{ title:"Let’s not let fear defeat our values",upvotes:2,source:"Medium",url:"https://medium.com/@sundar_pichai/let-s-not-let-fear-defeat-our-values-af2e5ca92371#.cfnq2efuy" },
+	{ title:"Google’s Angular 2 Framework Hits Beta",upvotes:3,source:"TechCrunch",url:"http://techcrunch.com/2015/12/15/googles-angular-2-framework-hits-beta/" },
+	{ title:"‘Star Wars: The Force Awakens’ Delivers the Thrills, With a Touch of Humanity",upvotes:5,source:"NY Times",url:"http://www.nytimes.com/2015/12/18/movies/star-wars-the-force-awakens-review.html?_r=0" }
 	];
 
 }]);

fbd80867484ed499d04c78e07102512621ff078d
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-17 11:11:22 -0600

    Fixed Links model import path in the Links controller

--- server/links/linksController.js
+++ server/links/linksController.js
@@ -1,7 +1,7 @@
 var Q = require('q');
 var request = require('request');
 
-var Links = require('./linkModel.js').Url;
+var Links = require('../db/configdb.js').Url;
 
 var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 

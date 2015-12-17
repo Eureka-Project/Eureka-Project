c343593772c044e2c428f109b65ac371d82a0732
Stoney McCrary <mccrary.stoney@gmail.com>
2015-12-17 10:15:35 -0600

    reformatted configdb.js

--- server/db/configdb.js
+++ server/db/configdb.js
@@ -1,16 +1,26 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://<Eureka>:<Eureka1?>@ds033175.mongolab.com:33175/eurekadb' || 'mongodb://localhost:27017');
 var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+	console.log('connection made');
+});
+
+
 
 var userSchema = mongoose.Schema({
 	username: String,
 	password: String,
 	firstname: String,
 	lastname: String,
-	date: { type: Date, default: Date.now }
+	date: { type: Date, default: Date.now },
+
 
 });
 
-var linkSchema = mongoose.Schema({
+var User = mongoose.model('User', userSchema);
+
+var urlSchema = mongoose.Schema({
 	title: String,
 	baseurl: String,
 	visits: Number,
@@ -18,13 +28,8 @@ var linkSchema = mongoose.Schema({
 
 });
 
-var user = mongoose.model('user', userSchema);
-var link = mongoose.model('link', linkSchema);
+var Url = mongoose.model('Url', urlSchema);
 
 
-mongoose.connect('mongodb://localhost:27017');
+module.exports = db;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(callback) {
-	console.log('connection made');
-});

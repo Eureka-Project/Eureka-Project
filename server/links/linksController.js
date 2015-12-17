aa5bd7e0d9faa052c9a760ce352d4e5e9cdbce43
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-17 11:06:16 -0600

    Updated Links/Url controller

--- server/links/linksController.js
+++ server/links/linksController.js
@@ -1,14 +1,14 @@
 var Q = require('q');
 var request = require('request');
 
-var Link = require('./linkModel.js');
+var Links = require('./linkModel.js').Url;
 
 var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
 
 
 module.exports = {
   findUrl: function (req, res, next, code) {
-    var findLink = Q.nbind(Link.findOne, Link);
+    var findLink = Q.nbind(Links.findOne, Links);
     findLink({code: code})
       .then(function (link) {
         if (link) {
@@ -24,7 +24,7 @@ module.exports = {
   },
 
   allLinks: function (req, res, next) {
-  var findAll = Q.nbind(Link.find, Link);
+  var findAll = Q.nbind(Links.find, Links);
 
   findAll({})
     .then(function (links) {
@@ -43,8 +43,8 @@ module.exports = {
       return next(new Error('Not a valid url'));
     }
 
-    var createLink = Q.nbind(Link.create, Link);
-    var findLink = Q.nbind(Link.findOne, Link);
+    var createLink = Q.nbind(Links.create, Links);
+    var findLink = Q.nbind(Links.findOne, Links);
 
     findLink({url: url})
       .then(function (match) {

ab2028f4f48819673393ed34a19d3e3e4207ac3c
Hank Couture <hankcouture@gmail.com>
2015-12-23 10:16:08 -0500

    Now able to click on user profiles and get unique url and profile data

--- public/app/controllers/profile.js
+++ public/app/controllers/profile.js
@@ -1,6 +1,6 @@
 angular.module('eureka.profile', [])
 
-.controller('ProfileController', ['$scope', '$http', '$window', '$location', 'Data', 'Auth' ,function($scope, $http, $window, $location, Data, Auth) {
+.controller('ProfileController', ['$scope', '$http', '$window', '$location', 'Data', 'Auth', '$stateParams' ,function($scope, $http, $window, $location, Data, Auth, $stateParams) {
 	// Checking If User Has Cookie
 	if (!Auth.isAuth()) $location.path('/login')
 
@@ -18,15 +18,42 @@ angular.module('eureka.profile', [])
 		}
 	}
 
+	// search function for search bar redirect
+	$scope.search = function(searchText) {
+		Data.searchValue = searchText;
+		$location.path('/search')
+	}
+
 	// data being temporarily stored
 	$scope.username = JSON.parse($window.localStorage.getItem('eureka')).username;
 	$scope.user_id = JSON.parse($window.localStorage.getItem('eureka')).user_id;
 	$scope.token = JSON.parse($window.localStorage.getItem('eureka')).token;
 	$scope.searchValue = Data.searchValue; // defined when 'search' is run
 
-	$scope.search = function(searchText) {
-		Data.searchValue = searchText;
-		$location.path('/search')
+	// profile data being temporarily stored
+	$scope.profileUsername = undefined; // defined when 'getProfileInfo' is run
+	$scope.profileFirstName = undefined; // defined when 'getProfileInfo' is run
+	$scope.profileLastName = undefined; // defined when 'getProfileInfo' is run
+
+
+	$scope.getProfileInfo = function() {
+		console.log('getting profile info...');
+		$http({
+			method: 'GET',
+			url: '/api/users/' + $stateParams.userID,
+		}).then(function (res) {
+			$scope.profileUsername = res.data.username
+			$scope.profileFirstName = res.data.firstname;
+			$scope.profileLastName = res.data.lastname;
+			return res.data;
+		}).catch(function (error) {
+			console.log(error);
+		})
 	}
+
+
+	// get profile info to display when controller loads
+	$scope.getProfileInfo();
+
 
 }]);

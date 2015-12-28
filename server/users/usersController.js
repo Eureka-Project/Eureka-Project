1b0487550a5c8a1287ec6b7692473822c5a52790
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-28 06:42:39 -0600

    Added comments to the users controller

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -6,33 +6,35 @@ var Users = require('../db/configdb.js').User;
 var Links = require('../links/linksController.js');
 var Upvotes = require('../upvotes/upvotesController.js');
 
-
 var secret = 'Festus is the bestest';
 
 exports = module.exports = {
 
+  // Promise version of Mongoose's 'Model.findOne()' method.
   findUser: Q.nbind(Users.findOne, Users),
   
+  // Promise version of Mongoose's 'Model.create()' method.
   createUser: Q.nbind(Users.create, Users),
 
+  // Return a unique token based on a user document from the database.
+  // When decoded later, it will serve as a conditions object for a
+  //   database query.
   genToken: function(user) {
     return jwt.encode({
-      user_id: user._id,
+      _id: user._id,
       username: user.username,
       firstname: user.firstname,
       lastname: user.lastname,
     }, secret);
   },
 
+  // Check whether the previously decoded token (stored as 'req.user')
+  //   is valid by seeing if it matches a document in the database.
   verifyToken: function (req, res, next) {
-    // checking to see if the user is authenticated
-    // grab the token in the header is any
-    // then decode the token, which we end up being the user object
-    // check to see if that user exists in the database
     if ( ! req.user ) {
       next(new Error('No token'));
     } else {
-      exports.findUser({ _id: req.user.user_id })
+      exports.findUser(req.user)
         .then(function(foundUser) {
           if (foundUser) {
             next();
@@ -45,7 +47,10 @@ exports = module.exports = {
         });
     }
   },
-  
+
+  // Login a user from a POST request containing a username and a password.
+  // If successful, respond with the username and user_id from the database
+  //   document as well as an access token.
   login: function (req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
@@ -53,10 +58,13 @@ exports = module.exports = {
     exports.findUser({ username: username })
       .then(function(user) {
         if ( ! user ) {
+          // Bad username
           throw new Error('User does not exist');
         } else if ( ! user.isPassword(password) ) {
+          // Bad password
           throw new Error('Incorrect password');
         } else {
+          // Success
           res.json({
             username: user.username,
             user_id: user._id,
@@ -69,19 +77,28 @@ exports = module.exports = {
       });
   },
 
+  // Signup a user from a POST request containing a username, password,
+  //   firstname, and lastname.
+  // If a database document with the same username already exists,
+  //   do not create a new user and respond that the username is taken.
+  // Create a new document in the database for the user and then
+  //   respond with the username and user_id of the new
+  //   document as well as an access token.
   signup: function (req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
 
-    // check to see if user already exists
+    // See if another user is already using the requested username.
     exports.findUser({ username: username })
       .then(function(user) {
         if (user) {
-          next(new Error('User already exist!'));
+          // Found a user with the same username.
+          // Abort signup and respond that the username is taken.
+          next(new Error('Username already taken. :('));
         } else {
-          // make a new user if not one
+          // Create a new user.
           return exports.createUser({
             username: username,
             password: password,
@@ -91,7 +108,9 @@ exports = module.exports = {
         }
       })
       .then(function(user) {
-        // create token to send back for auth
+        // Send back the new document's username and ObjectId
+        //   as well as a unique token for the document.
+
         // user example: {
         //   date: Fri Dec 18 2015 19:12:33 GMT-0600 (CST),
         //   _id: 5674af012e5833104b30ef0f,
@@ -114,14 +133,24 @@ exports = module.exports = {
       });
   },
 
+  // Return a promise for a database query.
+  // If the 'user_id' in the '/api/users/:user_id' uri
+  //   is NOT a Mongoose/Mongodb ObjectId,
+  //   assume it is a username and search the database for that username.
+  // If the 'user_id' in the uri IS a Mongoose/Mongodb ObjectId,
+  //   search the database for an ObjectId which matches it.
+  // If the uri does not contain a 'user_id',
+  //   use the one in the decoded token.
   findUserFromRequestHeaders: function(req) {
     return exports.findUser(
       ( ! mongoose.Types.ObjectId.isValid(req.params.user_id) )
         ? { username: req.params.user_id }
-        : { _id: req.params.user_id || req.user.user_id }
+        : { _id: req.params.user_id || req.user._id }
     );
   },
 
+  // Search the database for the requested user
+  //   and return the user's firstname, lastname, username, and ObjectId.
   getUserInfo: function (req, res, next) {
     exports.findUserFromRequestHeaders(req)
       .then(function(user) {
@@ -137,18 +166,33 @@ exports = module.exports = {
       });
   },
 
+  // Search the database for the requested user
+  //   and return the user's firstname, lastname, username, ObjectId
+  //   list of links created, and list of links upvoted.
+  //   and return the firstname, lastname, username, and ObjectId of the user
+  //   as well as the links created and upvoted by the user.
   getProfileInfo: function (req, res, next) {
+    // Declare an object which will store all of the response data.
+    var userData = {};
+
+    // Search the users collection in the database
+    //   and find the document for the user.
     exports.findUserFromRequestHeaders(req)
       .then(function(user) {
+        // Store the firstname, lastname, username, and ObjectId.
         userData = {
           firstname: user.firstname,
           lastname: user.lastname,
           username: user.username,
           user_id: user._id,
         };
+        // Search the upvotes collection in the database
+        //   and find the ObjectIds of the links which the user has upvoted.
         return Upvotes.findUpvotes({ user_id: userData.user_id });
       })  
       .then(function(upvotes) {
+        // Search the links collection in the database
+        //   and find the links which match the found ObjectIds.
         return Links.findLinks({
           _id: {
             $in: upvotes.map(function(upvote) {
@@ -158,11 +202,18 @@ exports = module.exports = {
         });
       })
       .then(function(upvotedLinks) {
+        // Store the links which the user has upvoted
+        //   in the response data object.
         userData.upvotedLinks = upvotedLinks
+        // Search the links collection in the database
+        //   and find the links which were submitted by the user.
         return Links.findLinks({ userid: userData.user_id });
       })
       .then(function(links) {
+        // Store the links which the user has created/submitted
+        //   in the response data object.
         userData.submittedLinks = links;
+        // Send the response data object to the client.
         res.json(userData);
       })
       .fail(function(error) {

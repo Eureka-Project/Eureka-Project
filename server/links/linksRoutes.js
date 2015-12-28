6192f39f62554dde7e148bb839e23fb98f943e45
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2015-12-27 22:48:32 -0600

    Removed unnecessary functions in the links controller and router

--- server/links/linksRoutes.js
+++ server/links/linksRoutes.js
@@ -1,18 +1,8 @@
 var linksController = require('./linksController.js');
 
 module.exports = function (app) {
-  // app === linkRouter injected from middleware.js
-
-  // app.param will hijack any request with a 'code' parameter on in
-  // like line 16 below. That code will actually be the shortned url
-  // so the real URL will be pre fetched from mongo and attached to
-  // req.navLink before it reaches line 16.
-  app.param('code', linksController.findUrl);
-
   app.route('/')
     .get(linksController.getPreviousThreeDaysLinks)
     .post(linksController.newLink);
-
-  app.get('/:code', linksController.navToLink);
 
 };

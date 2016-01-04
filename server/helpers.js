ce9f28ee718477f431a8a400640b5ac938c4212a
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-04 13:35:53 -0600

    Updated comment documentation in helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -22,23 +22,32 @@ exports = module.exports = {
   // Send a 'Forbidden' http response if it cannot be decoded.
   decodeToken: function (req, res, next) {
     var token = req.headers['x-access-token'];
+    // Example token:
     // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1Njg1OWM1NGRkYzUzNGQ3NWNiZThkMmUiLCJ1c2VybmFtZSI6Im1lbWUiLCJmaXJzdG5hbWUiOiJtZW1lIiwibGFzdG5hbWUiOiJtZW1lIn0.DxQxUgTw98PS8XVr2PeAIwRFntfOtkFZ8ZgTLKFDge4
+    
+    // If the request does not contain a token, set req.user to null
+    //   and skip to the next route.
     if( !token ) { 
       req.user = null;
       return next();
     }
+
+    // Try to decode the token.
     try {
       try {
+        // First try to use the secret for today.
         req.user = jwt.decode(token, secrets.today);
         req.makeNewToken = false;
         next();
       }
       catch(error) {
+        // If today's secret did not work, use yesterday's secret.
         req.user = jwt.decode(token, secrets.yesterday);
         req.makeNewToken = true;
         next();
       }
     } catch(error) {
+      // If neither secret worked, tell the client that the token is invalid.
       res.status(403).send({error: 'Invalid x-access-token'});
     }
   }

f8a7e5eb9b54e1b25b6dbc9908cf618351735640
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-01-04 13:47:46 -0600

    Added comment documentation to the upvotes controller

--- server/upvotes/upvotesController.js
+++ server/upvotes/upvotesController.js
@@ -11,10 +11,12 @@ exports = module.exports = {
 
   storeUpvote: Q.nbind(Upvote.create, Upvote),
 
+  // Upvote a link in the database.
   newUpvote: function(req, res, next) {
     var link_id = req.body.link_id || req.params.link_id;
     var user_id = req.body.user_id || req.user._id;
 
+    // Client needs to send both a link_id and user_id.
     if ( ! link_id ) {
       res.status(400);
       next(new Error('Did not receive link_id'))
@@ -23,20 +25,28 @@ exports = module.exports = {
       next(new Error('Did not receive user_id'))
     }
 
+    // Search the upvotes collection
+    //   to see whether the user has already upvoted this link.
     exports.findUpvote({
       link_id: link_id,
       user_id: user_id
-    }).then(function (upvote) {
+    }).then(function (upvote) { 
         var alreadyUpvoted = (
           upvote && 
           upvote.user_id && 
           upvote.user_id === user_id
         );
         if (alreadyUpvoted) {
+          // If the user has already upvoted this link,
+          //   change nothing in the database
+          //   and instead return the current data from the database.
           console.log('user %s has already upvoted %s', user_id, link_id);
           res.json(upvote);
           throw new Error('Stop promise chain');
         } else {
+          // Otherwise, connect this user with this link
+          //   in the upvotes collection
+          //   and continue down the promise chain.
           return exports.storeUpvote({
             user_id: user_id,
             link_id: link_id
@@ -44,24 +54,32 @@ exports = module.exports = {
         }
       })
       .then(function(upvote) {
+        // Find this link in the links collection.
         return Links.findLink({ _id: link_id })
       })
       .then(function(link) {
         if ( ! link ) {
+          // This should never run, since the client should only be sending
+          //   link_id's which the server has sent previously.
+          // But in case it does, tell the client that the link_id is invalid.
           var err = 'Link ' + link_id + ' does not exist';
           res.status(400);
           res.json({ error: err});
           console.log(err);
           throw new Error(err);
         } else {
+          // If the link exists, increase its upvote count by one,
+          //   and continue down the promise chain.
           link.upvotes++;
           return Q.nfcall(link.save, link);
         }
       })
       .then(function(link) {
+        // Send the updated link info to the client.
         res.json(link);
       })
       .fail(function (err) {
+        // Do nothing if the error was thrown to stop the promise chain.
         if (err.message !== 'Stop promise chain') {
           next(err);
         }

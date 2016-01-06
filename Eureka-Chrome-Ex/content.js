28c5b629ee2817c446a84cd6c08bdfffb949a336
Salman Oskooi <soskooi@gmail.com>
2016-01-06 16:48:52 -0600

    we have a functioning chrome extension! still working on UX

---
+++ Eureka-Chrome-Ex/content.js
@@ -0,0 +1,77 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+  var logged = false;
+  var loginHide = false;
+  $(".wrong").hide();
+  $("#addLink").hide();
+  $("#done").hide();
+
+  var getInfo = document.getElementById("login");
+  getInfo.addEventListener("submit", function(){
+    var password =  $('#password').val();
+    var username = $('#username').val();
+
+    $.ajax({
+      url: "http://localhost:4000/api/users/login",
+      type: "POST", 
+      data: {
+        "username": username, 
+        "password": password
+      },
+
+      success: function(res) {
+        chrome.storage.sync.set({"userData": res}, function() {
+          console.log("Settings saved");
+        });
+        loginHide = true;
+        logged = true;
+        $("#login").hide();
+        $("#addLink").show();
+        $("#please").hide();
+      },
+      error: function(err) {
+        console.log("error", JSON.stringify(err));
+        $(".wrong").show()
+      }
+    })
+    event.preventDefault();
+  })
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var addLinkButton = document.getElementById("addLink");
+  var linkSentHide = false;
+  addLinkButton.addEventListener("click", function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      
+      chrome.storage.sync.get("userData", function(data) {
+        
+        $.ajax({
+            url: "http://localhost:4000/api/links",
+            type: "POST",
+            data: {
+                    "username": data.userData.fullname,
+                    "url": tab.url,
+                    "user_id": data.userData.user_id
+            },
+            headers: {
+              "x-access-token": data.userData.token
+            },
+            success: function(res) {
+              $("#addLink").hide();
+              linkSentHide = true;
+              $("#done").show();
+            },
+            error: function(err) {
+              console.log(err);
+            }
+        });
+      });
+    })
+  }, false);
+}, false);
+
+
+
+

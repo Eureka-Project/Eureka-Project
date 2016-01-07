81684fac87ae5a02a7d0615097bdf1f087e29890
Daniel Sato <dksato@gmail.com>
2016-01-07 08:54:08 -0600

    Alchemy API key

--- server/links/links-Alchemy-APIkey.example.js
+++ server/links/links-Alchemy-APIkey.example.js
@@ -1,1 +1,2 @@
-module.exports = {APIkey:'4735a163ece4645c92ce8d16243895d9d64f1f07'}
+module.exports = {APIkey:'include Alchemy API key here'};
+// Fill this in and rename this file to links-Alchemy-APIkey.js
--- server/links/links-Alchemy-APIkey.js
+++
@@ -1,1 +0,0 @@
-module.exports = {APIkey:'4735a163ece4645c92ce8d16243895d9d64f1f07'}
